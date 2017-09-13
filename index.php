2fb96ee279848a63c77c346bbccaa3b6fb3cad96
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2017-09-13 23:56:26 +0200

    Daniel
    Grundgerüst

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php
+	$title = "soon - Dein persönlicher Kalender";
+?>
+
+<head>
+	<?php include 'head.php';?>
+</head>
+
+<body>
+	<div class="container">
+	<?php include 'navbar.php';?>
+	</div>
+</body>
+
+</html> 
