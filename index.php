4eb67d7ee10e3bbc92bfd80c6f108dc1215af734
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2017-09-16 19:56:44 +0200

    Daniel
    Inhaltliche Anpassungen in der Navigationleiste.
    
    Startseite (index.php) mit Hintergrundbild, sowie Anmelde- und Registrationbuttons ergänzt.

--- index.php
+++ index.php
@@ -12,19 +12,40 @@
 
 <body>
 	<?php include 'navbar.php';?>
+	<div class="background-image">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-md-3">
 			</div>
-			<div class="col-xs-12 col-md-3">
-				Registrieren
-			</div>
-			<div class="col-xs-12 col-md-3">
-				Anmelden
+			<div class="col-xs-12 col-md-6">
+				<div class="message">
+					Alle Ihre Termine – immer und überall abrufbar
+				</div>
 			</div>
 			<div class="col-xs-12 col-md-3">
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-3">
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<a href="registration.php">
+					<div class="linkbox registration">
+						Registrieren
+					</div>
+				</a>
+			</div>	
+			<div class="col-xs-6 col-md-3">
+				<a href="login.php">
+					<div class="linkbox login">
+						Anmelden
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-12 col-md-3">
+			</div>
+		</div>
+	</div>
 	</div>
 </body>
 
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php
+	$title = "Oktober 2017 - soon";
+?>
+
+<head>
+	<?php include 'head.php';?>
+</head>
+
+<body>
+	<?php include 'navbar.php';?>
+</body>
+
+</html> 
