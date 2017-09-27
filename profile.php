cf4922fce3ec6783e0a751470fc306086fe774c3
marcphpmaster <31928593+marcphpmaster@users.noreply.github.com>
2017-09-27 16:22:44 +0200

    Marc
    -Kleine Anpassung der Buttons
    -Entwurf Profil-Seite

--- profile.php
+++ profile.php
@@ -12,6 +12,31 @@
 
 <body>
 	<?php include 'navbar.php';?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-3">
+			</div>
+			<div class="col-xs-12 col-md-6">
+				<div class="box">
+					<h2>
+						Mein Profil
+					</h2>
+						<div class="edit-line">
+						Benutzername: <a href="index.php/edit.php">Benutzername ändern</a><br>
+						</div>
+						<div class="edit-line">
+						E-Mail: <a href="index.php/edit.php">E-Mail ändern</a><br>
+						</div>
+						<div class="edit-line">
+						<a href="index.php/edit.php">Passwort ändern</a><br>
+						</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-3">
+			</div>
+		</div>
+	</div>
 </body>
 
 </html> 
