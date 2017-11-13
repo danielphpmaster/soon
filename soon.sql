bc5368d77b75ce94276fcf6140c1543a2b55d41b
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2017-11-13 20:31:26 +0100

    Daniel
    Spalten-Typen geändert

--- soon.sql
+++ soon.sql
@@ -1,51 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Erstellungszeit: 11. Nov 2017 um 18:34
--- Server-Version: 10.1.28-MariaDB
--- PHP-Version: 7.1.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Datenbank: `soon`
---
 CREATE DATABASE IF NOT EXISTS `soon` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `soon`;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `appointments`
---
-
 CREATE TABLE `appointments` (
   `appointmentid` int(11) NOT NULL,
+  `appointmenttoken` text NOT NULL,
   `userid` int(11) NOT NULL,
-  `appointmentname` varchar(50) NOT NULL,
+  `appointmentname` text NOT NULL,
   `timestamp` int(11) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
-  `location` varchar(50) NOT NULL,
-  `comment` varchar(100) NOT NULL
+  `location` text NOT NULL,
+  `comment` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `users`
---
 
 CREATE TABLE `users` (
   `userid` int(10) UNSIGNED NOT NULL,
@@ -57,35 +33,17 @@ CREATE TABLE `users` (
   `created` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indizes der exportierten Tabellen
---
 
---
--- Indizes für die Tabelle `appointments`
---
 ALTER TABLE `appointments`
   ADD PRIMARY KEY (`appointmentid`);
 
---
--- Indizes für die Tabelle `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userid`);
 
---
--- AUTO_INCREMENT für exportierte Tabellen
---
 
---
--- AUTO_INCREMENT für Tabelle `appointments`
---
 ALTER TABLE `appointments`
   MODIFY `appointmentid` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT für Tabelle `users`
---
 ALTER TABLE `users`
   MODIFY `userid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
